e135a3cb96a2ba5e7eefae9cfca0d21c5c955e96
Nico091-2 <327nvelasco@frhsd.com>
2025-11-18 13:58:35 -0500

    dsd

---
+++ Js/java.js
@@ -0,0 +1,25 @@
+let myVariable;
+myVariable = "profileForm";
+
+const myelementsvariable = document.getElementById("output");
+console.log
+
+let age = document.getElementById("ageInput");
+
+let subbtn = document.getElementById("submitBtn");
+
+(document.querySelectorAll("input[type='radio']"));
+
+document.querySelectorAll("input[type='checkbox']");
+
+let Firstname = document.getElementById("firstName");
+let Lastname = document.getElementById("lastName");
+let email = document.getElementById("email");
+let ageInput = document.getElementById("ageInput");
+
+const element = document.getElementById("myBtn");
+element.addEventListener("click", getInfo);
+
+function getInfo() {
+    console.log(luck);
+}
