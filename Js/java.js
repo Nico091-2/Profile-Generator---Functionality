d8e8a40a955a4e2bbc43bcdc72e10845eeafcfe4
Nico091-2 <327nvelasco@frhsd.com>
2025-11-26 11:01:10 -0500

    ff

--- Js/java.js
+++ Js/java.js
@@ -2,21 +2,49 @@ let myVariable;
 myVariable = "profileForm";
 
 const nameText = document.getElementById("nameInput");
-console.log
 
 const age = document.getElementById("ageInput");
 const colorInput = document.getElementById("colorInput");
-
-(document.querySelectorAll("input[type='radio']"));
-
-document.querySelectorAll("input[type='checkbox']");
-
-const pet = document.getElementById("input[type='radio']");
-const hobby = document.getElementById("inpu[type='checkbox']");
 const submit = document.getElementById("submitBtn");
 
-submit.addEventListener('click', getInfo)
+submit.addEventListener('click', function () {
+
+    getInfo()
+    console.log(nameText.value);
+
+    output.innerHTML = 'Name:' + nameText + 'age:' + age + favPet
+
+});
+
+
+
+let pet = document.querySelectorAll('input[type="radio"]');
+let hobby = document.querySelectorAll('input[type="Checkbox"]');
+
+let favPet;
+let hobbies = [];
 
 function getInfo() {
-    console.log(nameText.value);
+    // reset hobbies array each tme value
+    hobbies = [];
+
+    // check for pets
+    for (let i = 0; i < pet.length; i++) {
+        if (pet[i].checked) {
+            favPet = pet[i].value;
+        }
+    }
+    console.log(favPet);
+
+    // check hobbies
+    for (let i = 0; i < hobby.length; i++) {
+        //check for pets
+        for (let i = 0; i < pet.length; i++) {
+            if (pet[i].checked) {
+                favPet = pet[i].value;
+            }
+        }
+        console.log(favPet);
+
+    }
 }
